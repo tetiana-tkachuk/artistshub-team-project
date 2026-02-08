415ce4f3e44eb22fb482e6138ce9769054ba03ec
tetiana-tkachuk <113261486+tetiana-tkachuk@users.noreply.github.com>
2026-02-08 23:12:38 +0000

    Deploying to gh-pages from @ tetiana-tkachuk/artistshub-team-project@5e3c2d5769cebea7624dbf544fa39e9c9653acc8 🚀

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import{a as f,R as L,S as w}from"./assets/vendor-Byf7b3WI.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const i of t)if(i.type==="childList")for(const c of i.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&o(c)}).observe(document,{childList:!0,subtree:!0});function s(t){const i={};return t.integrity&&(i.integrity=t.integrity),t.referrerPolicy&&(i.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?i.credentials="include":t.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function o(t){if(t.ep)return;t.ep=!0;const i=s(t);fetch(t.href,i)}})();const k=10,S=8,$="https://sound-wave.b.goit.study/api",v={GENRES:"/genres",ARTISTS:"/artists",ARTIST_BY_ID:"/artists/",FEEDBACKS:"/feedbacks"};f.defaults.baseURL=$;async function A(){const{data:e}=await f.get(`${v.FEEDBACKS}?limit=${k}&page=1`);return e}async function E(e){const{data:r}=await f.get(`${v.ARTISTS}?limit=${S}&page=${e}`);return r}function M(e){const r=Math.floor(e/6e4),s=Math.floor(e%6e4/1e3);return`${r}:${s.toString().padStart(2,"0")}`}function _(e){return e.intDiedYear===null?`${e.intFormedYear} - present`:`${e.intFormedYear} - ${e.intDiedYear}`}function B(e){return e.strBiographyEN.split(".").slice(0,2).join(".")+"."}function T(e){return e.genres.map(r=>`
+import{a as m,R as k,S as L,i as n}from"./assets/vendor-IF1acRtE.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))o(r);new MutationObserver(r=>{for(const a of r)if(a.type==="childList")for(const l of a.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&o(l)}).observe(document,{childList:!0,subtree:!0});function t(r){const a={};return r.integrity&&(a.integrity=r.integrity),r.referrerPolicy&&(a.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?a.credentials="include":r.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function o(r){if(r.ep)return;r.ep=!0;const a=t(r);fetch(r.href,a)}})();const $=10,S=8,A="https://sound-wave.b.goit.study/api",g={GENRES:"/genres",ARTISTS:"/artists",ARTIST_BY_ID:"/artists/",FEEDBACKS:"/feedbacks"};m.defaults.baseURL=A;async function E(){const{data:e}=await m.get(`${g.FEEDBACKS}?limit=${$}&page=1`);return e}async function M(e){const{data:s}=await m.get(`${g.ARTISTS}?limit=${S}&page=${e}`);return s}function _(e){const s=Math.floor(e/6e4),t=Math.floor(e%6e4/1e3);return`${s}:${t.toString().padStart(2,"0")}`}function T(e){return e.intDiedYear===null?`${e.intFormedYear} - present`:`${e.intFormedYear} - ${e.intDiedYear}`}function B(e){return e.strBiographyEN.split(".").slice(0,2).join(".")+"."}function q(e){return e.genres.map(s=>`
     <li>
-      <button class="artist-short-info-genre-btn">${r}</button>
-    </li>`).join("")}function q(e){return e.albumsList.map(s=>{const o=s.tracks.map(t=>{const i=t.movie===null?"":`
-        <a href="${t.movie}" target="_blank" rel="noopener noreferrer">
+      <button class="artist-short-info-genre-btn">${s}</button>
+    </li>`).join("")}function O(e){return e.albumsList.map(t=>{const o=t.tracks.map(r=>{const a=r.movie===null?"":`
+        <a href="${r.movie}" target="_blank" rel="noopener noreferrer">
           <svg width="24" height="24" fill="#ffffff">
             <use href="../img/sprite.svg#icon-youtube"></use>
           </svg>
         </a>`;return`
         <tr>
-          <td>${t.strTrack}</td>
-          <td>${M(t.intDuration)}</td>
-          <td>${i}</td>
+          <td>${r.strTrack}</td>
+          <td>${_(r.intDuration)}</td>
+          <td>${a}</td>
         </tr>`}).join("");return`
       <div class="artist-song-table">
-        <h3 class="artist-table-header">${s.strAlbum}</h3>
+        <h3 class="artist-table-header">${t.strAlbum}</h3>
         <table>
           <colgroup>
             <col style="width: 97px">
@@ -25,7 +25,7 @@ import{a as f,R as L,S as w}from"./assets/vendor-Byf7b3WI.js";(function(){const 
           </thead>
           <tbody>${o}</tbody>
         </table>
-      </div>`}).join("")}function O(e,r){const s=_(e);B(e);const o=T(e),t=q(e);r.insertAdjacentHTML("beforeend",`
+      </div>`}).join("")}function j(e,s){const t=T(e);B(e);const o=q(e),r=O(e);s.insertAdjacentHTML("beforeend",`
     <div class="artist-info-wrapper">
       <h1 class="artist-name-title">${e.strArtist}</h1>
       <div class="artist-info">
@@ -36,7 +36,7 @@ import{a as f,R as L,S as w}from"./assets/vendor-Byf7b3WI.js";(function(){const 
           <div class="artist-short-info-grid-wrapper">
             <div class="artist-short-info-years">
               <h3 class="short-info-small-header">Years active</h3>
-              <p class="short-info-small-text">${s}</p>
+              <p class="short-info-small-text">${t}</p>
             </div>
             <div class="artist-short-info-sex">
               <h3 class="short-info-small-header">Sex</h3>
@@ -65,32 +65,32 @@ import{a as f,R as L,S as w}from"./assets/vendor-Byf7b3WI.js";(function(){const 
       </div>
       <div class="artist-albums-wrapper">
         <h2 class="artist-albums-header">Albums</h2>
-        ${t}
+        ${r}
       </div>
       
-    </div>`)}const l=document.querySelector(".modal-overlay"),g=document.querySelector(".artists-modal-close-btn");document.querySelector(".modal");function m(e){e.target===l&&a()}function p(e){e.key==="Escape"&&a()}g.addEventListener("click",a);l.addEventListener("click",m);document.addEventListener("keydown",p);function a(){var e;l.classList.remove("is-open"),document.body.style.overflow="",(e=document.querySelector(".artist-info-wrapper"))==null||e.remove(),g.removeEventListener("click",a),l.removeEventListener("click",m),document.removeEventListener("keydown",p)}const n={list:document.querySelector(".js-artists-list"),loadMoreBtn:document.querySelector(".js-artists-load"),modalOverlay:document.querySelector(".modal-overlay"),modalRoot:document.querySelector(".modal"),modalOverlay:document.querySelector(".modal-overlay"),modalWindowCloseBtn:document.querySelector(".artists-modal-close-btn")};let d=1,u=!1;n.list&&n.loadMoreBtn&&j();function j(){n.list.innerHTML="",n.loadMoreBtn.hidden=!1,n.loadMoreBtn.addEventListener("click",x),n.list.addEventListener("click",R),y(!0)}async function x(){u||(d+=1,await y(!1))}async function y(e){u=!0,n.loadMoreBtn.textContent="Loading...",e&&(d=1);const s=(await E(d)).artists;e&&(n.list.innerHTML=""),n.list.insertAdjacentHTML("beforeend",s.map(C).join("")),s.length<8&&(n.loadMoreBtn.hidden=!0),n.loadMoreBtn.innerHTML=`
+    </div>`)}const d=document.querySelector(".modal-overlay"),y=document.querySelector(".artists-modal-close-btn");document.querySelector(".modal");function p(e){e.target===d&&c()}function h(e){e.key==="Escape"&&c()}y.addEventListener("click",c);d.addEventListener("click",p);document.addEventListener("keydown",h);function c(){var e;d.classList.remove("is-open"),document.body.style.overflow="",(e=document.querySelector(".artist-info-wrapper"))==null||e.remove(),y.removeEventListener("click",c),d.removeEventListener("click",p),document.removeEventListener("keydown",h)}const i={list:document.querySelector(".js-artists-list"),loadMoreBtn:document.querySelector(".js-artists-load"),modalOverlay:document.querySelector(".modal-overlay"),modalRoot:document.querySelector(".modal"),modalOverlay:document.querySelector(".modal-overlay"),modalWindowCloseBtn:document.querySelector(".artists-modal-close-btn")};let u=1,f=!1;i.list&&i.loadMoreBtn&&x();function x(){i.list.innerHTML="",i.loadMoreBtn.hidden=!1,i.loadMoreBtn.addEventListener("click",R),i.list.addEventListener("click",I),b(!0)}async function R(){f||(u+=1,await b(!1))}async function b(e){f=!0,i.loadMoreBtn.textContent="Loading...",e&&(u=1);const t=(await M(u)).artists;e&&(i.list.innerHTML=""),i.list.insertAdjacentHTML("beforeend",t.map(C).join("")),t.length<8&&(i.loadMoreBtn.hidden=!0),i.loadMoreBtn.innerHTML=`
     Load More
     <svg class="artists__load-icon" width="20" height="20" aria-hidden="true">
       <use href="../img/sprite.svg#icon-down-arrow-alt"></use>
     </svg>
-  `,u=!1}function C(e){var h;const r=e._id,s=e.strArtist,o=e.strBiographyEN||"",t=o.length>120?`${o.slice(0,120)}...`:o,i=((h=e.genres)==null?void 0:h.slice(0,4))||[];return`
+  `,f=!1}function C(e){var v;const s=e._id,t=e.strArtist,o=e.strBiographyEN||"",r=o.length>120?`${o.slice(0,120)}...`:o,a=((v=e.genres)==null?void 0:v.slice(0,4))||[];return`
     <li class="artist-card">
       <div class="artist-card__image">
-        <img src="${e.strArtistThumb||""}" alt="${s}" loading="lazy" />
+        <img src="${e.strArtistThumb||""}" alt="${t}" loading="lazy" />
       </div>
 
       <div class="artist-card__content">
         <ul class="artist-card__tags">
-          ${i.map(b=>`<li class="artist-card__tag">${b}</li>`).join("")}
+          ${a.map(w=>`<li class="artist-card__tag">${w}</li>`).join("")}
         </ul>
 
-        <h3 class="artist-card__name">${s}</h3>
-        <p class="artist-card__desc">${t}</p>
+        <h3 class="artist-card__name">${t}</h3>
+        <p class="artist-card__desc">${r}</p>
 
         <button
           class="artist-card__more js-artist-more"
           type="button"
-          data-artist-id="${r}"
+          data-artist-id="${s}"
         >
           Learn More
           <svg class="artist-card__icon" width="16" height="16" aria-hidden="true">
@@ -99,12 +99,12 @@ import{a as f,R as L,S as w}from"./assets/vendor-Byf7b3WI.js";(function(){const 
         </button>
       </div>
     </li>
-  `}async function R(e){const r=e.target.closest(".js-artist-more");if(!r)return;const s=r.dataset.artistId;s&&I(s)}async function I(e){var o;n.modalOverlay.classList.add("is-open"),document.body.style.overflow="hidden",(o=document.querySelector(".artist-info-wrapper"))==null||o.remove(),n.modalWindowCloseBtn.addEventListener("click",a),n.modalOverlay.addEventListener("click",m),document.addEventListener("keydown",p);const s=await(await fetch(`https://sound-wave.b.goit.study/api/artists/${e}/albums`,{headers:{Accept:"application/json"}})).json();O(s,n.modalRoot)}const F={feedbacksList:document.querySelector(".swiper-wrapper")};function N(e){const r=e.map(({_id:s,name:o,rating:t,descr:i})=>`
+  `}async function I(e){const s=e.target.closest(".js-artist-more");if(!s)return;const t=s.dataset.artistId;t&&F(t)}async function F(e){var o;i.modalOverlay.classList.add("is-open"),document.body.style.overflow="hidden",(o=document.querySelector(".artist-info-wrapper"))==null||o.remove(),i.modalWindowCloseBtn.addEventListener("click",c),i.modalOverlay.addEventListener("click",p),document.addEventListener("keydown",h);const t=await(await fetch(`https://sound-wave.b.goit.study/api/artists/${e}/albums`,{headers:{Accept:"application/json"}})).json();j(t,i.modalRoot)}const H={feedbacksList:document.querySelector(".swiper-wrapper")};function N(e){const s=e.map(({_id:t,name:o,rating:r,descr:a})=>`
         <div class="swiper-slide">
           <div class="feedback-block">
-            <div data-raty class="feedback-rating" data-id=${s} data-score=${t}></div>
-            <p class="feedback-descr">${i}</p>
+            <div data-raty class="feedback-rating" data-id=${t} data-score=${r}></div>
+            <p class="feedback-descr">"${a}"</p>
             <p class="feedback-name">${o}</p>
           </div>
-        </div>`).join("");F.feedbacksList.insertAdjacentHTML("beforeend",r)}function P(){const e={starOn:"./img/feedback-img/star-on.png",starOff:"./img/feedback-img/star-off.png",space:!1,readOnly:!0,halfShow:!1};Array.from(document.querySelectorAll("[data-raty]")).map(s=>{const o=Number(s.dataset.score);new L(s,{...e,score:o}).init()})}function H(){new w(".swiper",{direction:"horizontal",loop:!1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"},scrollbar:{el:".swiper-scrollbar"}})}D();async function D(){try{const r=(await A()).data;N(r)}catch(e){console.log("Error create feedbacks list",e);return}P(),H()}function Y(){const e=document.querySelector(".nav-button"),r=document.querySelector(".modal-close-button"),s=document.querySelector(".modal-menu");document.querySelectorAll(".modal-nav-link").forEach(t=>{t.addEventListener("click",()=>{s.classList.remove("modal-open")})}),e.addEventListener("click",()=>{s.classList.add("modal-open")}),r.addEventListener("click",()=>{s.classList.remove("modal-open")})}Y();const{height:G}=document.querySelector(".header").getBoundingClientRect();document.body.style.paddingTop=`${G}px`;
+        </div>`).join("");H.feedbacksList.insertAdjacentHTML("beforeend",s)}function P(){const e={starOn:"/img/feedback-img/star-on.png",starOff:"/img/feedback-img/star-off.png",space:!1,readOnly:!0,halfShow:!1};Array.from(document.querySelectorAll("[data-raty]")).map(t=>{const o=Number(t.dataset.score);new k(t,{...e,score:o}).init()})}function D(){new L(".swiper",{direction:"horizontal",loop:!1,autoHeight:!1,effect:"slide",pagination:{el:".swiper-pagination",type:"bullets"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev",addIcons:!1,enabled:!1},breakpoints:{768:{navigation:{enabled:!0}}}})}function Y(e,s="success"){const t={message:e,position:"topRight",timeout:5e3};switch(s){case"success":n.success(t);break;case"error":n.error(t);break;case"warning":n.warning(t);break;case"info":n.info(t);break;default:n.error({message:`Invalid type of toast: ${s}`,position:"topRight",timeout:5e3});break}}G();async function G(){try{const s=(await E()).data;N(s)}catch{Y("Error create feedbacks list","error");return}P(),D()}function K(){const e=document.querySelector(".nav-button"),s=document.querySelector(".modal-close-button"),t=document.querySelector(".modal-menu");document.querySelectorAll(".modal-nav-link").forEach(r=>{r.addEventListener("click",()=>{t.classList.remove("modal-open")})}),e.addEventListener("click",()=>{t.classList.add("modal-open")}),s.addEventListener("click",()=>{t.classList.remove("modal-open")})}K();const{height:z}=document.querySelector(".header").getBoundingClientRect();document.body.style.paddingTop=`${z}px`;
 //# sourceMappingURL=index.js.map
