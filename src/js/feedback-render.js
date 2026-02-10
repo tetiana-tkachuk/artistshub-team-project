8a0833038e6726278118b3f399816b0ed2d66e3e
Pitsyk Aleksandr <pitsyk.a@gmail.com>
2026-02-10 07:00:46 +0200

    update feedback star and pagination

--- src/js/feedback-render.js
+++ src/js/feedback-render.js
@@ -3,10 +3,13 @@ import refs from './refs';
 export function renderFeedbacks(feedbacks) {
   const feedbacksMarkup = feedbacks
     .map(({ _id, name, rating, descr }) => {
+      // Округлення рейтингу до цілого числа за правилами округлення 
+      const ratingRound = Math.round(rating);
+      // Створення розмітки
       return `
         <div class="swiper-slide">
           <div class="feedback-block">
-            <div data-raty class="feedback-rating" data-id=${_id} data-score=${rating}></div>
+            <div data-raty class="feedback-rating" data-id=${_id} data-score=${ratingRound}></div>
             <p class="feedback-descr">"${descr}"</p>
             <p class="feedback-name">${name}</p>
           </div>
@@ -14,5 +17,6 @@ export function renderFeedbacks(feedbacks) {
     })
     .join('');
 
+  // Додавання розмітки в DOM
   refs.feedbacksList.insertAdjacentHTML('beforeend', feedbacksMarkup);
 }
--- src/js/feedback.js
+++ src/js/feedback.js
@@ -5,12 +5,13 @@ import { getFeedbacks } from './api';
 import {renderFeedbacks} from './feedback-render';
 
 // Імпорт функцій - створення зірочок та сдайдеру для відгуків
-import {createLeverStar, createFeedbaсksSlider, setSliderNavigation } from './feedback-helpers';
+import {createLeverStar, createSlider, setSliderNavigation } from './feedback-helpers';
 
 import { showToast } from "./izitoast";
 
 // Запуск функції створення слайдеру
 createFeedbacksSlider();
+
 
 // ===========================================================================
 // Слухач події на зміну ширини екрана - для визначення кнопок навігації слайдера
@@ -40,8 +41,8 @@ export default async function createFeedbacksSlider() {
   createLeverStar();
 
   // Функція - створення Слайдеру відгуків
-  createFeedbaсksSlider();
-
+  createSlider();
+  
   // Функція - Перевірка розміру екрана та визначення кнопок навігації слайдера
   // ПРИМІТКА - Вимкнена, бо задіяні Responsive breakpoints Navigation Swiper
   // setSliderNavigation();
