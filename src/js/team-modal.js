b0999dbe89e8da0b42a45150238b24bbe3b2c17c
Anastasiia <anastasiia0matsiuk@gmail.com>
2026-02-22 19:12:30 +0200

    Add js team-modal

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -1,1 +1,22 @@
 const openTeamModalBtn = document.querySelector('.open-team-modal-btn');
+const closeTeamModalBtn = document.querySelector('.team-modal-close-btn');
+const teamModal = document.querySelector('.team-modal-overlay');
+
+function openModal() {
+    teamModal.classList.add('is-open');
+    document.body.style.overflow = 'hidden';
+}
+
+function closeModal() {
+    teamModal.classList.remove('is-open');
+    document.body.style.overflow = '';
+}
+
+openTeamModalBtn.addEventListener('click', openModal);
+closeTeamModalBtn.addEventListener('click', closeModal);
+
+teamModal.addEventListener('click', (e) => {
+    if (e.target === teamModal) {
+        closeModal();
+    }
+});
